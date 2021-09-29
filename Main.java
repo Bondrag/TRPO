fae6cffd76ab37e25ecefba407f03557a4840ea4
Bondrag <volodya.pavlyukov@mail.ru>
2021-09-29 14:37:52 +0300

    add Hello World

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+		System.out.println("Hello world!");
         ex1 e1 = new ex1();
         e1.proc1();
         ex2 e2 = new ex2();
