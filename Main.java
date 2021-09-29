01333d3366668325f1f45e1f3e22dbc00a023ef0
Bondrag <volodya.pavlyukov@mail.ru>
2021-09-29 14:43:11 +0300

    New branch - add ex1

--- Main.java
+++ Main.java
@@ -6,9 +6,5 @@ public class Main {
 		System.out.println("Hello world!");
         ex1 e1 = new ex1();
         e1.proc1();
-        ex2 e2 = new ex2();
-        e2.proc2();
-        ex3 e3 = new ex3();
-        e3.proc3();
     }
 }
---
+++ ex1.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class ex1 {
+    public void proc1(){
+        System.out.println("---Задание 1---");
+        int z=0,a=0,b=0,c=0;
+        a = 3;
+        System.out.println("Значение а - " + a);
+        b = 2;
+        System.out.println("Значение b - " + b);
+        c = 4;
+        System.out.println("Значение с - " + c);
+        z = ((a - 3)*b/2)+c;
+        System.out.println("Значение z равно - "+ z);
+    }
+}
