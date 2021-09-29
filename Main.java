c608b6ecc7b0946a031bab47716c993ab9aae880
Bondrag <80591707+Bondrag@users.noreply.github.com>
2021-09-29 15:14:18 +0300

    Site changes

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ public class Main {
 		System.out.println("Hello world!");
         ex1 e1 = new ex1();
         e1.proc1();
-		System.out.println("ex1 coming soon!");
+	System.out.println("ex1 coming soon!");
+	    System.out.println("This line was writen on GitHub");
     }
 }
