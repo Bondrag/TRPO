bd2426c69d8d9ed3a7402a920d43877e67de4435
Bondrag <volodya.pavlyukov@mail.ru>
2021-09-29 14:34:42 +0300

    Initial commit.

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ex1 e1 = new ex1();
+        e1.proc1();
+        ex2 e2 = new ex2();
+        e2.proc2();
+        ex3 e3 = new ex3();
+        e3.proc3();
+    }
+}
